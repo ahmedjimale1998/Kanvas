bf78ca0a699c7b795ed146252c7ca88bf7526ac2
ahmedjimale1998 <77024667+ahmedjimale1998@users.noreply.github.com>
2022-05-24 18:52:23 +0200

    mailservice.deploy and release added

--- MailService/Program.cs
+++ MailService/Program.cs
@@ -1,19 +1,52 @@
-using MailService;
+using MailService.Data;
+using MailService.Interfaces;
+using MailService.Repository;
+using Microsoft.EntityFrameworkCore;
 
-namespace MailService
+var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)
+   .Build();
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MailService", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddDbContext<MailContext>(options =>
+{
+    var connString = configuration.GetConnectionString("MyConnectionString");
+    options.UseNpgsql(connString);
+});
+
+builder.Services.AddTransient<IMailRepository, MailRepository>();
+
+// Add EF services to the services container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MailService");
+    });
+
 }
+app.UseRouting();
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
